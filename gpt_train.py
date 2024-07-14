f06a92b39a40dc17975e24f521c33b559d013de3
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-07-14 23:00:44 +0530

    Add package requirements.txt

--- gpt_train.py
+++ gpt_train.py
@@ -1,7 +1,3 @@
-# Copyright (c) Sebastian Raschka under Apache License 2.0 (see LICENSE.txt).
-# Source for "Build a Large Language Model From Scratch"
-#   - https://www.manning.com/books/build-a-large-language-model-from-scratch
-# Code: https://github.com/rasbt/LLMs-from-scratch
 
 import matplotlib.pyplot as plt
 import os
--- utils.py
+++ utils.py
@@ -1,20 +1,9 @@
-# Copyright (c) Sebastian Raschka under Apache License 2.0 (see LICENSE.txt).
-# Source for "Build a Large Language Model From Scratch"
-#   - https://www.manning.com/books/build-a-large-language-model-from-scratch
-# Code: https://github.com/rasbt/LLMs-from-scratch
-#
-# This file collects all the relevant code that we covered thus far
-# throughout Chapters 2-4.
-# This file can be run as a standalone script.
 
 import tiktoken
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-#####################################
-# Chapter 2
-#####################################
 
 
 class GPTDatasetV1(Dataset):
@@ -53,10 +42,6 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
 
     return dataloader
 
-
-#####################################
-# Chapter 3
-#####################################
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
@@ -113,9 +98,6 @@ class MultiHeadAttention(nn.Module):
         return context_vec
 
 
-#####################################
-# Chapter 4
-#####################################
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
